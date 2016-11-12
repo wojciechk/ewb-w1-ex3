46e44a803817a05a30c083a8022861f2a99e8749
Wojciech Kaminski <wojciechka@gmail.com>
2016-11-12 13:35:34 +0100

    work in progress

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
+var week1=(function ($) {
 
-var w1 = (function ($) {
-   
-    
+    var menu,
+        config={ "foo": "#", bar: "#" };
+        clicked=false;
 
+    function createMenu(cfg) {
+        var ul = $("<ul></ul>", {
+            "id": "menu",
+            "class": "dropdown-menu",
+            "css": {
+                "display": "none"
+            }
+        });
+
+
+        $.each(cfg, function (key, value) {
+            var li = $("<li></li>", {
+
+            });
+            var a = $("<a></a>", {
+                "text": value,
+                "href": key
+
+            })
+            li.append(a).appendTo(ul);
+        });
+        return ul;
+    }
+
+    menu=menu||createMenu(config);
+
+    var hbg = $("#my-hamburger");
+    hbg.append(menu);
+
+    hbg.on("click", function (e) {
+
+        
+    });
+return {
+    "menu":menu
+}
 
 })(jQuery);
